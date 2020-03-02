ff6f763c4199ea09bde92fd1496720411b04d068
brandonfeist <feist.brandon@gmail.com>
2020-03-01 23:25:35 -0800

    Add status update loop for now.

---
+++ mining-bot/robotStatus.lua
@@ -0,0 +1,5 @@
+component.modem.open(80)
+while true do
+    local e = {event.pull('modem_message')}
+    print('State: '..e[6]..', X,Y,Z,Direction: '..e[7]..', message: '..e[8]..', energyPercent: '..e[9]..', time: '..e[10])
+end
